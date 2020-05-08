b170e182a8e1755e5ba56129b668367afe01cda9
Alexander Zwierzynski <alexanderzwierzynski@Alexanders-MacBook-Pro.local>
2020-05-08 19:36:16 +0200

    test

--- src/uniqueIds.js
+++ src/uniqueIds.js
@@ -1,4 +1,4 @@
- /*
+/*
  |--------------------------------------------------------------------------
  | Unique Ids
  |--------------------------------------------------------------------------
@@ -24,183 +24,175 @@
  | setInitial(str) - sets inital id string value
  | reset(str) - resets current id string (optional by given string)
  |
- */
+*/
 
 
 /**
- * Initial first id
- * Needs to have a min length of 3
- *
- * @var string
- */
+* Initial first id
+* Needs to have a min length of 3
+*
+* @var string
+*/
 let inital = 'aaa'
 
 
 /**
- * Unique Ids object
- */
-const UniqueIds = {
+ * Sets initial first id
+ *
+ * @var string
+*/
+exports.setInitial = function(str){
+  if( str.length > 2 ){
+    inital = str
+    last = str
+  }
+}
 
 
-	/**
-	 * Sets initial first id
-	 *
-	 * @var string
-	 */
-	setInitial: function(str){
-		if( str.length > 2 ){
-			inital = str
-			this.last = str
-		}
-	},
+/**
+ * Stores Last provided id
+ *
+ * @var string (this.init = default)
+*/
+let last = inital
 
 
-	/**
-	 * Stores Last provided id
-	 *
-	 * @var string (this.init = default)
-	 */
-	last: inital,
+/**
+ * Counter for provided ids
+ *
+ * @var number
+*/
+let cnt = 0
 
 
-	/**
-	 * Counter for provided ids
-	 *
-	 * @var number
-	 */
-	cnt: 0,
+/**
+ * Returns new unique id
+ *
+ * @param none
+ * @return string id
+*/
+exports.make = function(){
 
+  // Split last id into single chars
+  let l = last.split('')
+  // n = length of last string
+  let n = l.length - 1
 
-	/**
-	 * Returns new unique id
-	 *
-	 * @param none
-	 * @return string id
-	 */
-	make: function(){
+  let updated = 0;
+  let cur, next
 
-		// Split last id into single chars
-		let l = this.last.split('');
-		// n = length of last string
-	 	let n = l.length - 1;
+  // Start with last char.
+  // If there's a next character
+  // in char depot, last letter
+  // will be replaced.
+  // 'abc' -> 'abd'
+  // When there's no next char
+  // the leftern char will be updated
+  // and so on.
+  // 'abz' -> 'aca'
+  while (!updated && n > -1) {
+    // Set next char
+    next = getNext(l[n], n)
+    if( next ){
+      // We've found next.
+      // Update id
+      l[n] = next
+      updated = 1
+      // Set all following to first char
+      // e.g. 'abz' -> 'aca'
+      // e.g. 'azz' -> 'baa'
+      let nn = n + 1;
+      if( typeof l[nn] !== 'undefined' ){
+        for(let i=nn; i < l.length; i++){
+          l[i] = getFirst()
+        }
+      }
+    }
+    n--
+  }
 
-	 	let updated = 0;
-	 	let cur, next;
+  if( !updated ){
+    // No character was updated, no new id was found
+    // extend id length by 1, add first character
+    // e.g. ZZZ -> ZZZa
+    l.push(getFirst())
+  }
 
-	 	// Start with last char.
-	 	// If there's a next character
-	 	// in char depot, last letter
-	 	// will be replaced.
-	 	// 'abc' -> 'abd'
-	 	// When there's no next char
-	 	// the leftern char will be updated
-	 	// and so on.
-	 	// 'abz' -> 'aca'
-		while (!updated && n > -1) {
-			// Set next char
-			next = getNext(l[n], n);
-			if( next ){
-				// We've found next.
-				// Update id
-				l[n] = next;
-				updated = 1;
-				// Set all following to first char
-				// e.g. 'abz' -> 'aca'
-				// e.g. 'azz' -> 'baa'
-				let nn = n + 1;
-				if( typeof l[nn] !== 'undefined' ){
-					for(let i=nn; i < l.length; i++){
-						l[i] = getFirst();
-					}
-				}
-			}
-			n--;
-		}
+  // Convert char arr back to string
+  last = l.join('')
 
-		if( !updated ){
-			// No character was updated, no new id was found
-			// extend id length by 1, add first character
-			// e.g. ZZZ -> ZZZa
-			l.push(getFirst());
-		}
+  // Update counter
+  cnt++
 
-		// Convert char arr back to string
-		this.last = l.join('');
-
-		// Update counter
-		this.cnt++;
-
-		// Return new id
-		return this.last;
-
-
-	 	/**
-	   * Returns first character
-	   * of Chars
-	   *
-	   * @param none
-	   * @return string first char
-	   * @private
-	   */
-	  function getFirst(){
-	  	return getChars().substr(0,1);
-	  }
-
-
-	  /**
-	   * Returns next char after given char
-	   *
-	   * @param string char
-	   * @param number index if current char in id
-	   * @return string next char
-	   * @private
-	   */
-		 function getNext(char, n){
-			 let index = getChars().indexOf(char)
-			 let ip = index + 1
-			 let next = getChars().split('')[ip]
-			 if(typeof next !== 'undefined' ){
-				 if( n === 0 && !isNaN(parseInt(next)) ){
-					 // We're dealing with the first position
-					 // of id. There are no numbers allowed
-					 // Return false
-					 return false
-				 }
-				 return next
-			 }
-			 return false
-		 }
-
-
-		/**
-	   * String of characters
-	   * as depot for ids
-	   *
-	   * @param none
-	   * @return {string} Chars
-	   * @private
-	  */
-		function getChars(){
-			return 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ'
-		}
-	},
-
-	/**
-	 * Sets last id to it's inital value (default = 'aaa')
-	 * or to a given string
-	 *
-	 * @param string str (optional)
-	 * @return nothing
-	 */
-	reset: function(str){
-		if( typeof str !== 'undefined' ){
-			if( str.length > 2 ){
-				this.setInitial(str)
-			}
-		}
-		this.last = inital
-	}
+  // Return new id
+  return last
 
 }
 
-module.exports = UniqueIds
+
+/**
+ * Sets last id to it's inital value (default = 'aaa')
+ * or to a given string
+ *
+ * @param string str (optional)
+ * @return nothing
+*/
+exports.reset = function(str){
+  if( typeof str !== 'undefined' ){
+    if( str.length > 2 ){
+      this.setInitial(str)
+    }
+  }
+  last = inital
+}
+
+
+/**
+ * Returns first character
+ * of Chars
+ *
+ * @param none
+ * @return string first char
+ * @private
+*/
+function getFirst(){
+  return getChars().substr(0,1);
+}
+
+
+/**
+ * Returns next char after given char
+ *
+ * @param string char
+ * @param number index if current char in id
+ * @return string next char
+ * @private
+*/
+function getNext(char, n){
+  let index = getChars().indexOf(char)
+  let ip = index + 1
+  let next = getChars().split('')[ip]
+  if(typeof next !== 'undefined' ){
+    if( n === 0 && !isNaN(parseInt(next)) ){
+      // We're dealing with the first position
+      // of id. There are no numbers allowed
+      // Return false
+      return false
+    }
+    return next
+  }
+  return false
+}
+
+
+/**
+* String of characters
+* as depot for ids
+*
+* @param none
+* @return {string} Chars
+* @private
+*/
+function getChars(){
+ return 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ'
+}
