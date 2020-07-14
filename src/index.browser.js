59403244322599169d6f80de0022817cc74b94df
Alexander Zwierzynski <alexanderzwierzynski@Alexanders-MBP.fritz.box>
2020-07-14 20:22:40 +0200

    clean up

--- src/index.browser.js
+++ src/index.browser.js
@@ -1,3 +1,11 @@
+/**
+ * UniqeIds
+ * Endless unique ids.
+ *
+ * @author   WWAZ <https://github.com/WWAZ>
+ * @license  NoLicense
+ */
+
 const uniqueIds = require('../src/uniqueIds')
 
 if( typeof window !== 'undefined' ){
