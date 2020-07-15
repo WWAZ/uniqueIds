efa6fa29026089aa6777a6d5dcc5831c47c23eea
Alexander Zwierzynski <alexanderzwierzynski@Alexanders-MacBook-Pro.local>
2020-07-15 09:02:53 +0200

    clean up

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/**
+ * UniqeIds
+ * Endless unique ids.
+ *
+ * @author   WWAZ <https://github.com/WWAZ>
+ * @license  NoLicense
+ */
+
 const uniqueIds = require('./src/uniqueIds')
+
+if( window ){
+  window['UniqueIds'] = uniqueIds
+}
+
 
 module.exports = uniqueIds
